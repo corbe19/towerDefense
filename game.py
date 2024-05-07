ee404e48d501adf10bf32f66d19d4cb5e5e222a3
Luke Corbett <corbettluke14@gmail.com>
2024-05-07 14:23:38 -0600

    added background support

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 class Game:
     def __init__(self):
@@ -10,14 +11,19 @@ class Game:
         self.money = 100
         self.lives = 100
         self.towers = []
-        self.bg = pygame.image.load("assets/Background1.png")
+        self.bg = pygame.image.load(os.path.join("game_assets", "Background1.png"))
 
     def run(self):
         run = True
-
+        clock = pygame.time.Clock()
         while run:
+            clock.tick(60)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
         
         pygame.quit()
+
+    def draw(self):
+        self.win.blit(self.bg, (0, 0))
+        pygame.display.update()
