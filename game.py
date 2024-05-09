06ace4da1971bf31df498836863f1bc3be70c6a4
Luke Corbett <corbettluke14@gmail.com>
2024-05-08 21:18:42 -0600

    removed click detection for enemy pathing

--- game.py
+++ game.py
@@ -4,14 +4,16 @@ import os
 class Game:
     def __init__(self):
         self.running = True
-        self.width = 1280
-        self.height = 720
+        self.width = 1200
+        self.height = 800
         self.win = pygame.display.set_mode((self.width, self.height))
         self.enemies = []
         self.money = 100
         self.lives = 100
         self.towers = []
         self.bg = pygame.image.load(os.path.join("game_assets", "Background1.png"))
+        self.bg = pygame.transform.scale(self.bg, (self.width, self.height))
+       
 
     def run(self):
         run = True
@@ -21,9 +23,19 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
+
+                pos = pygame.mouse.get_pos()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pass
+            
+            self.draw()
         
         pygame.quit()
 
     def draw(self):
         self.win.blit(self.bg, (0, 0))
         pygame.display.update()
+
+g = Game()
+g.run()
