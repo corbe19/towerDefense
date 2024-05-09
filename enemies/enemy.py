8e4fe9f719463b923bff0440a8d59c9d14478df1
Luke Corbett <corbettluke14@gmail.com>
2024-05-09 12:59:34 -0600

    started on move function

--- enemies/enemy.py
+++ enemies/enemy.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 class Enemy:
     img = []
@@ -12,6 +13,7 @@ class Enemy:
         self.path = [(15, 436), (191, 435), (197, 205), (436, 205), (444, 509), (753, 514), (756, 363), (1178, 356), (1190, 357)]
         self.path_pos = 0
         self.vel = 3
+        self.move_count = 0
 
     def draw(self, win):
         # draw enemy
@@ -20,9 +22,33 @@ class Enemy:
 
     def move(self):
         # move enemy
-        win.blit(self.img, (self.x, self.y))
-        self.move()
-        pass
+        x1, y1 = self.path[self.path_pos]
+        if self.path_pos + 1 >= len(self.path):
+            x2, y2 = (1190, 357)
+        else:
+            x2, y2 = self.path[self.path_pos + 1]
+
+        move_dis = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+        self.move_count += 1
+        dirvect = (x2 - x1, y2 - y1)
+
+        move_x, move_y = (self.x + dirvect[0] * self.move_count, self.y + dirvect[1] * self.move_count)
+        self.dis += math.sqrt((move_x - x1)**2 + (move_y - y1)**2)
+
+        #next path point
+        if self.dis >= move_dis:
+            self.dis = 0
+            self.move_count = 0
+            self.path_pos += 1
+            if self.path_pos >= len(self.path):
+                self.path_pos = 0
+                self.x, self.y = self.path[0]
+                return
+
+        self.x = move_x
+        self.y = move_y
+
 
     def collide(self, x, y):
         # check if enemy is hit/range of tower
